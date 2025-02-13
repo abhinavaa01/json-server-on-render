fd61a124f1638348ded67e02eab7dd1744ce63bb
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-02-13 16:27:58 +0530

    CORS policy updated

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
-
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 server.listen(port);
